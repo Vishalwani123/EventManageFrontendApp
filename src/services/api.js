37e063f57be3cf50862c77c93c916e65ee056e42
Vishalwani123 <93905384+Vishalwani123@users.noreply.github.com>
2026-02-10 00:06:14 +0530

    API updated

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // const API_URL = 'https://eventmanagebackendapp-production.up.railway.app';
 // const API_URL = 'http://127.0.0.1:54444';
- const API_URL = 'http://localhost:8080';
-//const API_URL = 'https://event-management-system-tgp9.onrender.com';
+//  const API_URL = 'http://localhost:8080';
+const API_URL = 'https://event-management-system-tgp9.onrender.com';
 // const API_URL = process.env.REACT_APP_BACKEND_URL;
 // const API_URL = 'http://127.0.0.1:59001';
 
